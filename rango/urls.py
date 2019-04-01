ddba293d3cf4bbc9911818b27f280f2de4fdbf87
d.regular <mr.d.regular@protonmail.com>
2019-04-01 19:31:02 +0300

    Added possibilities adding Category, Page

--- rango/urls.py
+++ rango/urls.py
@@ -4,6 +4,9 @@ from rango import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('add_category/', views.add_category, name='add_category'),
+    re_path('^category/(?P<category_name_slug>[\w\-]+)/add_page/$',
+            views.add_page, name='add_page'),
     re_path('^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category,
          name='show_category'),
 ]
