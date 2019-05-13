83926643c14eb99b0864e30ef952608ec0e42835
d.regular <mr.d.regular@protonmail.com>
2019-05-13 19:28:31 +0300

    Added possibility user authentication with help Django-Registration-Redux

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -13,11 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
+from registration.backends.simple.views import RegistrationView
+
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/'
+
 
 urlpatterns = [
+    path('accounts/register/', MyRegistrationView.as_view(), name='registration_register'),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
 ]
+
